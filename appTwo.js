d5638abd4e0dae6f09ec59de8bd7c3198bf5a187
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-05 14:55:33 +0000

    process console

--- appTwo.js
+++ appTwo.js
@@ -1,5 +1,8 @@
-console.log("Node");
-console.warn("error...");
-console.error("Error, Error");
-console.error(new Error("¡We have an Error!"));
+//console.log("Node");
+//console.warn("error...");
+//console.error("Error, Error");
+//console.error(new Error("¡We have an Error!"));
 //console.log(process);
+//console.log(process.env);
+//console.log(process.argv);
+//console.log(process.memoryUsage());
