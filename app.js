c7bb2464a07cc32ed46bd1a861bee6cad603db3c
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-05 13:23:46 +0000

    modules exports

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
-import { hello } from "./modules/hello.js";
+//** ===  === === > Modules Require == Import < === === ===  */
+//*TODO === === ===  Modules === */
+/* import { hello } from "./modules/hello.js"; */
+const helloFriend = require("./modules/hello.js");
 
-console.log(hello("Luisa"));
+//*! === Call Modules ===  */
+console.log(helloFriend);
+console.log(helloFriend.hello("Luisa"));
+console.log(helloFriend.myGame("Zelda"));
--- modules/hello.js
+++ modules/hello.js
@@ -1,3 +1,11 @@
-export const hello = (name) => {
+const hello = (name) => {
   return `Hello, my friend ${name}`;
 };
+
+const myGame = (game) => {
+  return `My favorite game is: ${game}`;
+};
+
+//** === Modules Exports ===  */
+module.exports.hello = hello;
+module.exports.myGame = myGame;
