c40a3f54cd6a82d2a680cf9a1dd8cf26162d7605
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-05 14:20:03 +0000

    modules 03

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 //** ===  === === > Modules Require == Import < === === ===  */
 //*TODO === === ===  Modules === */
-const helloFriend = require("./modules/hello.js");
+//const helloFriend = require("./modules/hello.js");
+const { hello, myGame } = require("./modules/hello.js");
 //import { call } from "./modules/call.js";
 
 //*! === Call Modules ===  */
-console.log(helloFriend);
-console.log(helloFriend.hello("Luisa"));
-console.log(helloFriend.myGame("Zelda"));
+//console.log(helloFriend);
+/* console.log(helloFriend.hello("Luisa"));
+console.log(helloFriend.myGame("Zelda")); */
+console.log(hello("Luisa"));
+console.log(myGame("Zelda"));
 //console.log(call("Madre"));
