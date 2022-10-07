4412291132c67c637b1a19ffcbf653fb853d6afa
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-07 13:18:27 +0000

    node 02

---
+++ concept/monohilo.js
@@ -0,0 +1,12 @@
+console.log("Hola Mundo");
+
+let i = 0;
+
+setInterval(() => {
+  console.log(i);
+  i++;
+
+  //i == 5 ? console.log("Llegamos") : console.log("Error");
+}, 1000);
+
+console.log("Finished");
