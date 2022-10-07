ff0430a3a994a7c33618f647ad3f0e2e372a08f5
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-07 16:53:00 +0000

    new name promises

---
+++ async/promises.js
@@ -0,0 +1,42 @@
+//** === Promises */
+/* const hello = (name) => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log(`Hello, my name is ${name}`);
+      resolve(name);
+    }, 1000);
+  });
+};
+
+hello("Gala")
+  .then((name) => console.log("Juliette"))
+  .catch((err) => console.log("We have an error")); */
+
+//*! === */
+
+/* const peoples = (people) => {
+  return new Promise((resolve, reject) => {
+    resolve(people);
+    reject(error);
+  });
+};
+
+peoples()
+  .then((people) => console.log("Alma"))
+  .catch((err) => console.log("error"));
+ */
+//*! ===  */
+const friends = (friend) => {
+  return new Promise((resolve, reject) => {
+    console.log(`First Friend: ${friend}`);
+    setTimeout(() => {
+      resolve(friend);
+      //reject(friend);
+    }, 1000);
+    console.log("Other friends come in half hour");
+  });
+};
+
+friends("Mauricio")
+  .then((friend) => console.log(`He will forever be my best friend ${friend}`))
+  .catch((err) => console.log("Not my friend"));
