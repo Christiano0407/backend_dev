5897a8f077d09e827301c4f02be9b04120ea9c82
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-10 14:38:56 +0000

    tables js

---
+++ modules/console.js
@@ -0,0 +1,20 @@
+let tables = [
+  {
+    a: 4,
+    b: 10,
+  },
+];
+
+console.table(tables);
+
+let tablesTwo = [
+  {
+    name: "Luisa",
+    fullName: "Evans",
+    year: 24,
+    city: "New York",
+    school: "Art School Art",
+  },
+];
+
+console.table(tablesTwo);
