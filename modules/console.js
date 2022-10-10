b11f6ae6b9f765b6b9dc53c47f653e29ce23ea2a
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-10 14:42:24 +0000

    console02

--- modules/console.js
+++ modules/console.js
@@ -18,3 +18,19 @@ let tablesTwo = [
 ];
 
 console.table(tablesTwo);
+
+/*
+console.log: recibe cualquier tipo y lo muestra en el consola.
+console.info: es equivalente a log pero es usado para informar.
+console.error: es equivalente a log pero es usado para errores.
+console.warn: es equivalente a log pero es usado para warning.
+console.table: muestra una tabla a partir de un objeto.
+console.count: inicia un contador autoincremental.
+console.countReset: reinicia el contador a 0.
+console.time: inicia un cronometro en ms.
+console.timeEnd: Finaliza el cronometro.
+console.group: permite agrupar errores mediante identación.
+console.groupEnd: finaliza la agrupación.
+console.clear: Limpia la consola.
+
+*/
