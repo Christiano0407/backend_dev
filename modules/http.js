adb5a480bd57e7bbd32258a38eba95dc045cbba7
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-11 13:15:38 +0000

    new http

--- modules/http.js
+++ modules/http.js
@@ -2,3 +2,25 @@
 const http = require(`http`);
 
 //*! ===  Request & Response / Server and Client === */
+http.createServer(router).listen(3000);
+
+function router(req, res) {
+  console.log("New Request!");
+  console.log(req.url);
+
+  switch (req.url) {
+    case "/hola":
+      res.write(`Hello, World! What up?`);
+      res.end();
+      break;
+
+    default:
+      res.write("Error, 404! Not Exist!");
+      res.end();
+  }
+
+  res.writeHead(201, { "Content-Type": "text/plain" });
+  // === Write User Request
+  /* res.write("Hello World! My First Server Request!");
+    res.end(); */
+}
