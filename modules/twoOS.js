99d0abd63bd64f2fa8027951ef9d640b659d19c3
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-11 13:41:01 +0000

    process

---
+++ modules/twoOS.js
@@ -0,0 +1,18 @@
+//** === === OS === sYSTEM Operative === */
+const os = require("os");
+
+//console.log(os.type());
+//console.log(os.arch());
+//console.log(os.platform());
+//console.log(os.cpus());
+//console.log(os.constants);
+//console.log(os.freemem());
+/* function gb(bytes) {
+  return mb(bytes) / SIZE;
+}
+console.log(mb(os.freemem()));
+console.log(gb(os.freemem())); */
+//console.log(os.homedir());
+//console.log(os.tmpdir());
+//console.log(os.hostname());
+console.log(os.networkInterfaces());
