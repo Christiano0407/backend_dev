96e28a967b3b8b9a1e9ab9e19e8002d33864af06
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-17 16:31:24 +0000

    buffer memory

---
+++ memory/buffer.js
@@ -0,0 +1,9 @@
+//*TODO === === === Buffer => Space In Memory === === ===  */
+
+let buffer = Buffer.from("Hello");
+
+let byte = Buffer.alloc(4);
+
+//*! === */
+console.log(buffer.toString());
+console.log(byte);
