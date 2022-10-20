e08f2281e4ab3573d19461d5e739d7f0b1e3a68c
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-20 13:44:48 +0000

    set Timer

--- node/time/timer.js
+++ node/time/timer.js
@@ -15,3 +15,22 @@ setTimeout(newCourse, 2000, "ExpressJs");
 setTimeout(() => {
   console.log("New Function");
 }, 3000);
+
+//*? === setInterval() === setInterval(code, delay) */
+//*? === setInterval() === setInterval(func, delay, arg0, arg1, /* … ,*/ argN) */
+
+const intervalHero = (hero) => {
+  setInterval(() => {
+    console.log(`My hero ${hero}`);
+  }, 1000);
+};
+
+//intervalHero("Wolverine");
+
+setInterval(
+  (movie) => {
+    console.log(`New Movie ${movie}`);
+  },
+  1000,
+  "Thor"
+);
