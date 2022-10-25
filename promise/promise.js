9a2647370dbb69db1f26debe775e56e4ebf36675
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-25 13:20:39 +0000

    promises part 02

--- promise/promise.js
+++ promise/promise.js
@@ -11,6 +11,36 @@ const myPromise = new Promise((resolve, reject) => {
   }, 2000);
 });
 
-myPromise
-  .then((result) => console.log(result))
-  .catch((error) => console.log(error));
+const resolvePromises = (result) => {
+  console.log(result);
+};
+
+const rejectPromises = (error) => {
+  console.log(error);
+};
+
+myPromise.then(resolvePromises).catch(rejectPromises);
+
+//** === === === === Exercise PIZZA === === === ===   */
+const statusPizza = () => {
+  return Math.random() < 0.8;
+};
+
+const callPizza = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (statusPizza()) {
+      resolve("Pizza, on this moment!!");
+    } else {
+      reject("Sorry!! Not Pizza!");
+    }
+  }, 2000);
+});
+
+const truePizza = (messageValue) => {
+  console.log(messageValue);
+};
+const falsePizza = (messageError) => {
+  console.log(messageError);
+};
+
+callPizza.then(truePizza).catch(falsePizza);
