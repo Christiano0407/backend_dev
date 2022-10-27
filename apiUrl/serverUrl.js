ca15ecf4e19b361762a502d2f30435a640210fb1
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-27 16:41:55 +0000

    first server

--- apiUrl/serverUrl.js
+++ apiUrl/serverUrl.js
@@ -6,7 +6,32 @@ const PORT = 3000;
 const server = http.createServer((req, res) => {
   //req.end("Hello World");
   const { method } = req;
+
+  if (method === "GET") {
+    return requestGet(req, res);
+  } else {
+    console.log(`Method not found with server ${method}`);
+  }
 });
+
+const requestGet = (req, res) => {
+  const path = req.url;
+
+  if (path === "/") {
+    res.statusCode = 200;
+    res.end(`Welcome,to my first Server and API, create with Node.js`);
+  }
+
+  if (path === "/courses") {
+    res.statusCode = 200;
+    res.end(JSON.stringify(courses.infoCourses));
+  }
+
+  if (path === "/courses/programming") {
+    res.statusCode = 200;
+    res.end(JSON.stringify(courses.infoCourses.programming));
+  }
+};
 
 server.listen(PORT, () => {
   console.log(`Listen on port http://localhost:${PORT}...`);
