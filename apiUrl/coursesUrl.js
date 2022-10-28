c07751c2125772bd2528741066729801b4b9500e
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-28 18:21:42 +0000

    full data base

--- apiUrl/coursesUrl.js
+++ apiUrl/coursesUrl.js
@@ -49,6 +49,158 @@ let infoCourses = {
       level: `Basic`,
     },
   ],
+  data: [
+    {
+      id: 1,
+      title: "Basic SQL",
+      language: "PHP",
+      level: "Basic",
+    },
+    {
+      id: 2,
+      title: "PostgreSQL",
+      language: "PHP",
+      level: "Medium",
+    },
+    {
+      id: 3,
+      title: "Basic R",
+      language: "Python",
+      level: "Expert",
+    },
+  ],
+  python: [
+    {
+      id: 1,
+      title: "Fundaments of Python",
+      language: "Python",
+      level: "Basic",
+    },
+    {
+      id: 2,
+      title: "Basic Terminal",
+      language: "Git",
+      level: "Basic",
+    },
+    {
+      id: 3,
+      title: "Basic Git & GitHub",
+      language: "Git",
+      level: "Basic",
+    },
+    {
+      id: 4,
+      title: "Algorithm with Python",
+      language: {
+        basic: "PHP",
+        medium: ["SQL", "MySQL", "PostgreSQL", "Python"],
+        expert: "R",
+      },
+      level: "Medium",
+    },
+  ],
+  developer: [
+    {
+      basic: [
+        {
+          id: 1,
+          title: "Basic HTML",
+          language: "HTML",
+          level: "Basic",
+        },
+        {
+          id: 2,
+          title: "Basic CSS",
+          language: "CSS",
+          level: "Basic",
+        },
+        {
+          id: 3,
+          title: "Basic HTML & CSS",
+          language: ["HTML", "CSS"],
+          level: "Basic",
+        },
+      ],
+    },
+    {
+      medium: [
+        {
+          id: 1,
+          title: "Basic Javascript",
+          language: "Javascript",
+          level: "medium",
+        },
+        {
+          id: 2,
+          title: "Backend with Node Js",
+          language: "Javascript",
+          level: "medium",
+        },
+        {
+          id: 3,
+          title: "Frontend & Backend",
+          language: [
+            "HTML",
+            "CSS",
+            "Javascript",
+            "NodeJs",
+            "Git",
+            "GitHub",
+            "Npm",
+          ],
+          level: "Medium",
+        },
+      ],
+    },
+    {
+      expert: [
+        {
+          id: 1,
+          title: "Full React Js",
+          language: ["Javascript", "NodeJs", "ExpressJs"],
+          frameworks: [
+            {
+              basicFrameworks: ["Webpack", "Jamstack", "ViteJs", "NextJS"],
+            },
+          ],
+          level: "Expert",
+        },
+        {
+          id: 2,
+          title: "Full VueJs",
+          language: ["Javascript", "NodeJs", "ExpressJs"],
+          frameworks: [
+            {
+              basicFrameworks: ["Webpack", "Jamstack", "ViteJs", "NextJS"],
+            },
+          ],
+          level: "medium",
+        },
+        {
+          id: 3,
+          title: "Full Angular",
+          language: ["Javascript", "Node Js", "ExpressJs"],
+          frameworks: [
+            {
+              basicFrameworks: ["Webpack", "Jamstack", "ViteJs", "NextJS"],
+            },
+          ],
+          level: "Expert",
+        },
+        {
+          id: 4,
+          title: "Bootcamp Developer",
+          language: ["ReactJs", "VueJs", "Angular", "Svelte"],
+          frameworks: [
+            {
+              basicFrameworks: ["Webpack", "Jamstack", "ViteJs", "NextJS"],
+            },
+          ],
+          level: "Expert",
+        },
+      ],
+    },
+  ],
 };
 
 //console.log(infoCourses);
