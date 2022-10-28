3de4834e727ff1fda68df6333185005e207bc66e
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-28 13:33:22 +0000

    status 200

--- apiUrl/serverUrl.js
+++ apiUrl/serverUrl.js
@@ -12,38 +12,52 @@ const server = http.createServer((req, res) => {
   } else {
     console.log(`Method not found with server ${method}`);
   }
-});
 
+  if (method === "POST") {
+    return methodPost(req, res);
+  } else {
+    console.log(`Method not found with server ${method}`);
+  }
+});
+//** ===> status 200 >> Default === */
 const requestGet = (req, res) => {
   const path = req.url;
 
   if (path === "/") {
     res.statusCode = 200;
-    res.end(`Welcome,to my first Server and API, create with Node.js`);
+    return res.end(`Welcome,to my first Server and API, create with Node.js`);
   }
 
   if (path === "/courses") {
     res.statusCode = 200;
-    res.end(JSON.stringify(courses.infoCourses));
+    return res.end(JSON.stringify(courses.infoCourses));
   }
 
   if (path === "/courses/programming") {
     res.statusCode = 200;
-    res.end(JSON.stringify(courses.infoCourses.programming));
+    return res.end(JSON.stringify(courses.infoCourses.programming));
   }
 
   if (path === "/courses/math") {
     res.statusCode = 200;
-    res.end(JSON.stringify(courses.infoCourses.math));
+    return res.end(JSON.stringify(courses.infoCourses.math));
   }
 
   if (path === "/courses/cloud") {
     res.statusCode = 200;
-    res.end(JSON.stringify(courses.infoCourses.cloud));
+    return res.end(JSON.stringify(courses.infoCourses.cloud));
   }
 
   res.statusCode = 404;
-  res.end("Sorry! Web & Resource Not Found...");
+  return res.end("Sorry! Web & Resource Not Found...");
+};
+
+const methodPost = (req, res) => {
+  const path = req.url;
+  if (path === "/courses/programming") {
+    res.statusCode = 200;
+    return res.end("Server add new request POST courses/programming");
+  }
 };
 
 server.listen(PORT, () => {
