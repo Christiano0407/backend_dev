622f33c3722b51b626b7dbc1b249a8c5fbf1f50a
Chris_Velázquez04 <placebo0704@gmail.com>
2022-10-31 16:00:00 +0000

    server Express

--- express/exServer.js
+++ express/exServer.js
@@ -1,13 +1,17 @@
 //** === === Express Server Part Two === === */
 const express = require(`express`);
 const app = express();
-const PORT = 3000;
 
-//** === Call APP === */
+const { infoCourses } = require("../apiUrl/coursesUrl");
+console.log(infoCourses);
+const PORT = process.env.PORT || 3000;
+
+//** === Call APP & Routing === */
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Hello World!!");
 });
 
-//** === === === PORT */
-app.listen(PORT);
-console.log(`Listen App in Port http://localhost:${PORT}`);
+//** === === === PORT & Listen */
+app.listen(PORT, () => {
+  console.log(`Listen App in Port http://localhost:${PORT}...`);
+});
